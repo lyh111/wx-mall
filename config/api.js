347a21cf5de37b02b63903d43b5fe754dcb6c6af
zw-pc3 <1486724367@qq.com>
2019-07-24 16:51:19 +0800

    init

--- config/api.js
+++ config/api.js
@@ -1,5 +1,5 @@
 var NewApiRootUrl = 'http://192.168.0.104:8080/platform-framework/api/';
-var test = 'bbb';
+var test = 'aaa';
 
 module.exports = {
     IndexUrl: NewApiRootUrl + 'index/index', //首页数据接口
